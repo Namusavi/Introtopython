22d3459f5a5d28d88dc0e5cc8fd22f81b6e2654e
Quintonyatich7533 <amusavikhamasi@gmail.com>
2023-06-27 10:10:40 +0300

    betting system statement

---
+++ bettingsystem.py
@@ -0,0 +1,14 @@
+user = int(input("Enter any number between 1 to 5"))
+
+if user == 1:
+    print("You have Won a trip")
+elif user == 2:
+    print("You have won a boat")
+elif user == 3:
+    print("You have won free shopping")
+elif user == 4:
+    print("You have won a Jaguar F-pace")
+elif user == 5:
+    print("You have won free foreign supplies")
+else:
+    print("invalid number entered")
