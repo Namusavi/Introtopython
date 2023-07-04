5e3e4a6eec4de80427dfc977bd2e397863c48542
Quintonyatich7533 <amusavikhamasi@gmail.com>
2023-07-04 10:21:00 +0300

    summaryoop

--- oop/summaryoop.py
+++ oop/summaryoop.py
@@ -1,1 +1,9 @@
 #oop is a paradigm or a way of writing code that makes it more reusable
+#we have classes and objects
+#class is like a blueprint
+#objects - instance of a class
+
+#inheritance
+#polymorphism
+#abstraction
+#encapsulation
