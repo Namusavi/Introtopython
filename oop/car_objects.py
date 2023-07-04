5cb679ed7301647ab4f0ea467f58b61cd8c33011
Quintonyatich7533 <amusavikhamasi@gmail.com>
2023-07-04 09:42:34 +0300

    car objects

---
+++ oop/car_objects.py
@@ -0,0 +1,11 @@
+from Car import Car
+
+owner1 =Car("Toyota", "Vitz", "Kim", "black")
+owner2 =Car("Toyota", "Vitz", "Abel", "red")
+owner3 =Car("Toyota", "Vitz", "Josh", "blue")
+owner4 =Car("Toyota", "Vitz", "Kibe", "yellow")
+
+print(owner1.make, owner1.model, owner1.owner, owner1.color)
+print(owner2.make, owner2.model, owner2.owner, owner2.color)
+print(owner3.make, owner3.model, owner3.owner, owner3.color)
+print(owner4.make, owner4.model, owner4.owner, owner4.color)
